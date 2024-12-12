090b1a4bfd679d2ddb2c36ddfd75ac9ad08cf2f8
Courtlyn Boykin <courtlynb@google.com>
2024-12-12 11:56:20 -0500

    Create to do card and to do list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.pixelplanner"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.pixelplanner"
---
+++ app/src/main/java/com/example/pixelplanner/model/ToDoItem.kt
@@ -0,0 +1,8 @@
+package com.example.pixelplanner.model
+
+data class ToDoItem (
+    val id: Int,
+    val title: String,
+    val description: String,
+//    val dueDate
+)
